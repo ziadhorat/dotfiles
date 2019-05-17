a7de709a44ecf53728ded68b3183433ec977720f
Zia <ziadhorat@gmail.com>
2019-05-17 22:52:48 +0200

    sh trevor.sh should install

--- trevor.sh
+++ trevor.sh
@@ -1,58 +1,25 @@
-#!/usr/bin/sh
-# Option based dot-file installer
+#!/usr/bin/sh 
 
-if [ $# -eq 0 ]
-then
-        echo "Missing options!"
-        echo "(run $0 -h for help)"
-        exit 0
-fi
-
-ECHO="false"
-GLOB="false"
-
-while getopts "hmu" OPTION; do
-        case $OPTION in
-                m)
-                        ECHO="true1"
-                        ;;
-                u)
-			ECHO="true2"
-			;;
-                h)
-                        echo "Usage:"
-                        echo "$0 -m "
-                        echo "$0 -u "
-                        echo ""
-                        echo "   -m     For OSx devices (iMacs/MacBooks)"
-                        echo "   -u     For Ubuntu"
-                        exit 0
-                        ;;
-        esac
-done
-
-if [ $ECHO = "true1" ]
-then
-    echo "-----Mac specific-----";
+OS="`uname`"
+case $OS in
+  'Linux')
+    echo "----- Linux -----";
+    ;;
+  'Darwin')
+    echo "----- MacOS -----";
     osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true';
     echo "DarkMode";
-    GLOB="true";
-fi
+    ;;
+  *) ;;
+esac
 
-if [ $ECHO = "true2" ]
-then
-    echo "-----Ubuntu specific-----";
-    GLOB="true";
-fi
+echo "----- Global -----";
+cp -R .vim ~/;
+cp .vimrc ~/;
+echo ".vimrc";
+cp .gitignore ~/;
+git config --global core.excludesfile ~/.gitignore;
+echo ".gitignore";
 
-if [ $GLOB = "true" ]
-then
-    echo "-----Global settings-----";
-    cp -R .vim ~/;
-    cp .vimrc ~/;
-    echo ".vimrc";
-    cp .gitignore ~/;
-    git config --global core.excludesfile ~/.gitignore;
-    echo ".gitignore";
-    exit 0
-fi
+echo "----- Finished -----";
+exit 0
