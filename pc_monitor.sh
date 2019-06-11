f28d90b21768382adb4c942736414822902cb753
Zia <ziadhorat@gmail.com>
2019-06-11 17:49:16 +0200

    Revert "Update pc_monitor.sh"
    This reverts commit be2e450eee36a64e6f5c87db69d5dafa1b7c25f8.

--- pc_monitor.sh
+++ pc_monitor.sh
@@ -2,17 +2,21 @@
 #Dependency: run "brew install coreutils"
 #run "sh pc_monitor c4r10"
 
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+BOLD=`tput bold`
+NC=`tput sgr0`
+
 counter=0
 seat=1
 
 while [ ${counter} -le 10 ]
 do
 	if gtimeout 0.01 ping $1s${seat} -c 1 | grep -q "64"
-		then printf "⭕"
-		else printf "❌"
+		then echo "$1s${seat} :\t${GREEN}${BOLD}ALIVE${NC}"
+		else echo "$1s${seat} :\t${RED}${BOLD}DOWN${NC}"
 	fi
 	((seat++))
 	((counter++))
 	sleep 1
 done
-printf "\n"
